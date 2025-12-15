db8ab4cf0922f5463a031839f579a5a5653b75d1
explosionnnn <167171727+explosionnnn@users.noreply.github.com>
2025-12-15 21:54:07 +0800

    Add files via upload

--- DS1_HW5.cpp
+++ DS1_HW5.cpp
@@ -29,6 +29,16 @@ struct Main {
     bool is_legend;
 };
 
+struct Rec1 {
+    int num;
+    std::string name;
+    std::string type1;
+    int total;
+    int hp;
+    int atk;
+    int def;
+}
+
 class System {
     private:
         std::vector<Main> main_list;
@@ -39,6 +49,21 @@ class System {
         System() {
             main_list_size = 0;
             tree_height = 0;
+        }
+
+        int binarySearch(int target) {
+            int l = 0, r = main_list.size() - 1;
+
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (main_list[mid].num == target)
+                    return mid;
+                else if (main_list[mid].num < target)
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+            return -1; // 找不到
         }
 
         Bst* Add(int num, int hp, Bst *tree) {
@@ -79,6 +104,28 @@ class System {
             }
         }
 
+        int FindInRange(int num1, int num2, Bst *tree, std::vector<int> &list) {
+            Bst *cur = tree;
+            int count = 0;
+            int cur_hp = cur -> list[0].hp;
+
+            if (num1 > cur_hp) {
+                count += FindInRange(num1, num2, tree->left, list); //先找左
+            }
+
+            if (num1 <= cur_hp && num2 >= cur_hp) {
+                for (int i = 0; i < cur -> list.size(); i++) {
+                    list.push_back(cur -> list[0].num);
+                }
+            }
+
+            if (num2 < cur_hp) {
+                count += FindInRange(num1, num2, tree->right, list); //找右
+            }
+
+            return count;
+        }
+
         void Task1() {
             ReadFile(); //havn't design
             main_list_size = main_list.size();
@@ -86,5 +133,20 @@ class System {
                 tree = Add(main_list[i].num, main_list[i].hp, tree);
             }
             tree_height = Height(tree);
+        }
+
+        void Task2(int num1, int num2) {
+            std::vector<int> list;
+            int index;
+            if (num1 > num2) {
+                int tmp = num2;
+                num2 = num1;
+                num1 = tmp;
+            }
+            int count_visited = FindInRange(num1, num2, tree, list);
+            for (int i = 0; i < list.size(); i++) {
+                index = binarySearch(list[i]);
+                PrintTask2(main_list[index]); //hasn't design
+            }
         }
 };
