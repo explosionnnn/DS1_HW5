3bb2d80d626c5049f9f3a3f69df97eee81b8afe4
explosionnnn <167171727+explosionnnn@users.noreply.github.com>
2025-12-16 12:04:56 +0800

    Add files via upload

--- DS1_HW5.cpp
+++ DS1_HW5.cpp
@@ -37,13 +37,75 @@ struct Rec1 {
     int hp;
     int atk;
     int def;
-}
+};
+
+
+class BinarySearchTree {
+    private:
+        Bst *tree;
+    public:
+        BinarySearchTree() {
+            tree = nullptr;
+        }
+
+        Bst* GetTree() {
+            return tree;
+        }
+
+        Bst* GetRight() {
+            return tree->right;
+        }
+        Bst* GetLeft() {
+            return tree->left;
+        }
+        std::vector<Number> GetNode() {
+            return tree -> list;
+        }
+
+
+        void SetTree(Bst* tree) {
+            this -> tree = tree;
+        }
+
+        bool IsNull() {
+            if (tree == nullptr) {
+                return true;
+            }
+            return false;
+        }
+
+        int Height(Bst* t) {
+            Bst* cur = t;
+            if (cur == NULL) {
+                return 0;
+            }
+            int right = Height(cur -> right);
+            int left = Height(cur -> left);
+            if (right > left) {
+                return right+1;
+            } else {
+                return left+1;
+            }
+        }
+
+        Bst* FindMax(Bst *cur) {
+            if (cur == nullptr) {
+                return cur;
+            }
+            while (cur -> right != nullptr) {
+                cur = cur -> right;
+            }
+
+            return cur;
+        }
+
+};
 
 class System {
     private:
         std::vector<Main> main_list;
         int main_list_size;
-        Bst *tree;
+        BinarySearchTree bst;
         int tree_height;
     public:
         System() {
@@ -67,7 +129,7 @@ class System {
         }
 
         Bst* Add(int num, int hp, Bst *tree) {
-            Bst *cur = tree;
+            Bst *cur = bst.GetTree();
             if (cur == nullptr) { // i.e., BST points to "empty"
                 Bst *new_node = new Bst;
                 Number n = {num, hp};
@@ -77,10 +139,10 @@ class System {
                 cur = new_node;
                 return cur;
             } else if (cur -> list[0].hp < hp) {
-                cur -> right = Add(num, hp, cur -> right); // store A on the right subtree
+                cur -> right = Add(num, hp, cur -> left); // store A on the right subtree
                 return cur;
             } else if (cur -> list[0].hp > hp) {
-                cur -> left = Add(num, hp, cur -> left);
+                cur -> left = Add(num, hp, cur -> right);
                 return cur;
             } else if (cur -> list[0].hp == hp) {
                 Number n = {num, hp};
@@ -90,27 +152,13 @@ class System {
             return cur;
         }
 
-        int Height(Bst *tree) {
-            Bst *cur = tree;
-            if (cur == NULL) {
-                return 0;
-            }
-            int right = Height(cur -> right);
-            int left = Height(cur -> left);
-            if (right > left) {
-                return right+1;
-            } else {
-                return left+1;
-            }
-        }
-
         int FindInRange(int num1, int num2, Bst *tree, std::vector<int> &list) {
             Bst *cur = tree;
             int count = 0;
             int cur_hp = cur -> list[0].hp;
 
             if (num1 > cur_hp) {
-                count += FindInRange(num1, num2, tree->left, list); //先找左
+                count += FindInRange(num1, num2, tree->right, list); //先找右
             }
 
             if (num1 <= cur_hp && num2 >= cur_hp) {
@@ -120,7 +168,7 @@ class System {
             }
 
             if (num2 < cur_hp) {
-                count += FindInRange(num1, num2, tree->right, list); //找右
+                count += FindInRange(num1, num2, tree->left, list); //找左
             }
 
             return count;
@@ -128,22 +176,24 @@ class System {
 
         void Task1() {
             ReadFile(); //havn't design
+            Bst* cur = bst.GetTree();
             main_list_size = main_list.size();
             for (int i = 0; i < main_list_size; i++) {
-                tree = Add(main_list[i].num, main_list[i].hp, tree);
+                cur = Add(main_list[i].num, main_list[i].hp, cur);
             }
-            tree_height = Height(tree);
+            bst.Height(bst.GetTree());
         }
 
         void Task2(int num1, int num2) {
             std::vector<int> list;
             int index;
+            Bst* cur = bst.GetTree();
             if (num1 > num2) {
                 int tmp = num2;
                 num2 = num1;
                 num1 = tmp;
             }
-            int count_visited = FindInRange(num1, num2, tree, list);
+            int count_visited = FindInRange(num1, num2, cur, list);
             for (int i = 0; i < list.size(); i++) {
                 index = binarySearch(list[i]);
                 PrintTask2(main_list[index]); //hasn't design
